329d7c5943bf385463b2c37c00cec451ee46d012
SKooFi <98702476+SkooFi@users.noreply.github.com>
2023-05-29 16:29:08 +0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,13 +1,13 @@
 fx_version 'cerulean'
 game 'gta5' 
 
-author 'sjpfeiffer'
-description 'A ped spawner for fivem'
+author 'SKooFi#3565'
+description 'Script By : SKooFi#3565'
 version '1.2.0'
 
 lua54 'yes'
 
 client_scripts {
 	'config.lua',
-	'client/main.lua'
+	'main.lua'
 }
